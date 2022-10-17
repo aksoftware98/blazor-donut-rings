5cfa1d5216ef0ee4309ce64c645cab0d2d129219
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-10-17 07:48:59 +0400

    Create the basics and the structure of the ring component

---
+++ src/AKSoftware.Components.Blazor.DonutRings/Ring.razor.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace AKSoftware.Components.Blazor
+{
+    public partial class Ring
+    {
+        #region Parameters 
+        /// <summary>
+        /// Set gradient color for the stroke of the ring, null for no gradient and will use SolidColor property 
+        /// </summary>
+        [Parameter]
+        public GradientColor? RingGradient { get; set; }
+
+        /// <summary>
+        /// Set gradient color for the full track ring, null for no gradient and will use TrackRingSolidColor property 
+        /// </summary>
+        [Parameter]
+        public GradientColor? TrackRingGradient { get; set; }
+
+        /// <summary>
+        /// Indicate if the full track ring is needed
+        /// </summary>
+        [Parameter]
+        public bool WithTrackRing { get; set; }
+
+        /// <summary>
+        /// Represents the solid color of the progress ring 
+        /// </summary>
+        [Parameter]
+        public string RingSolidColor { get; set; } = "#008AF6";
+        
+        /// <summary>
+        /// Represents the solid color of the full track ring
+        /// </summary>
+        [Parameter]
+        public string TrackRingSolidColor { get; set; } = "#e5e5e5";
+
+        /// <summary>
+        /// Represents the max value of the progress 
+        /// </summary>
+        [Parameter]
+        public double MaxValue { get; set; } = 100;
+
+        /// <summary>
+        /// Represents the value of the progress 
+        /// </summary>
+        [Parameter]
+        public double Value { get; set; } = 0;
+
+        /// <summary>
+        /// Represents the width of the ring container square
+        /// Default value 100px
+        /// </summary>
+        [Parameter]
+        public string Width { get; set; } = "100px";
+
+        /// <summary>
+        /// Represents the height of the ring container square
+        /// Default value 100px
+        /// </summary>
+        [Parameter]
+        public string Height { get; set; } = "100px";
+        #endregion 
+
+    }
+
+    /// <summary>
+    /// Represents a gradient color to be used for a ring 
+    /// For version 0.1 it supports only linear gradient consists of two values (start and end)
+    /// IMPORTANT! to be upgraded to support more complex gradients
+    /// </summary>
+    public struct GradientColor
+    {
+        public GradientColor(string? firstColor, string? secondColor)
+        {
+            if (string.IsNullOrWhiteSpace(firstColor))
+                throw new ArgumentNullException(nameof(firstColor));
+
+            if (string.IsNullOrWhiteSpace(secondColor))
+                throw new ArgumentNullException(nameof(secondColor));
+            FirstColor = firstColor;
+            SecondColor = secondColor;
+        }
+
+        public string FirstColor { get; set; }
+        public string SecondColor { get; set; }
+    }
+}
