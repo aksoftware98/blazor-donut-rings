70873ec6f8590be8771f2afd5d812ab1fb58e4ac
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-11-13 02:53:12 +0400

    Create the shared project and some DTOs in the system

---
+++ src/AKSoftware.ProductivePlus.Shared/Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKSoftware.ProductivePlus.Shared
+{
+    public class Activity
+    {
+
+        public int TotalUnits { get; set; }
+
+        public DateTime? StartingDate { get; set; }
+
+        public DateTime? EndingDate { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public string? Notes { get; set; }
+
+
+
+    }
+
+}
