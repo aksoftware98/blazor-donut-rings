6abd4fbbe22f61d56e256de11cf8286e576dc840
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-11-13 14:33:36 +0400

    Clean up some files and create the client services project

---
+++ src/AKSoftware.ProductivePlus.Client.Services/DependencyInjectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKSoftware.ProductivePlus.Client.Services
+{
+    internal class DependencyInjectionExtensions
+    {
+    }
+}
