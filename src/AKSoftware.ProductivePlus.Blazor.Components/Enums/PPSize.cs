1ddf13be67fc0f6c459705affcd9c3723d9bd36f
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-11-24 13:15:08 +0400

    Add the Microsoft Fluent Icons

---
+++ src/AKSoftware.ProductivePlus.Blazor.Components/Enums/PPSize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKSoftware.ProductivePlus.Blazor.Components.Enums
+{
+	public enum PPSize
+	{
+		Small,
+		Medium,
+		Large,
+	}
+}
